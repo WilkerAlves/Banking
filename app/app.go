2f72a7109b8b2a99e00d10e0bba0cd34afd889c5
Wilker Alves <wilkeralves97@gmail.com>
2021-09-11 12:43:13 -0300

    configurando .env

--- app/app.go
+++ app/app.go
@@ -1,14 +1,25 @@
 package app
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/wilker/banking/domain"
 	"github.com/wilker/banking/service"
 	"log"
 	"net/http"
+	"os"
 )
 
+func sanityCheck() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Environment variable not defined")
+	}
+}
+
 func Start() {
+	sanityCheck()
+
 	router := mux.NewRouter()
 
 	// wiring
@@ -20,5 +31,8 @@ func Start() {
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 
 	// starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", router))
+	address := os.Getenv("SERVER_ADDRESS")
+	port := os.Getenv("SERVER_PORT")
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router))
 }
